2d68a0ce0b72760610db40e1b65a8e16b26517ee
NjunkoDenis <njunko.denis@gmail.com>
2017-02-22 16:34:06 +0300

    create UserRepository

---
+++ server/src/main/java/com/grsu/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.grsu.repository;
+
+import com.grsu.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by DENIS on 22.02.2017.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String username);
+}
