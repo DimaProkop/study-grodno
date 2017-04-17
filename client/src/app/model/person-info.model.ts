19cdd3e73185990389f3f21614b07e128469c5e4
NjunkoDenis <njunko.denis@gmail.com>
2017-04-17 22:57:50 +0300

    update branch

---
+++ client/src/app/model/person-info.model.ts
@@ -0,0 +1,28 @@
+/**
+ * Created by DENIS on 17.04.2017.
+ */
+
+export class PersonInfoModel {
+
+  id: number;
+
+  firstName: string;
+
+  lastName: string;
+
+  middleName: string;
+
+  gender: string;
+
+  dateOfBirth: string;
+
+  phone: string;
+
+  motivationLetter: string;
+
+  citizenship: string;
+
+  levelOfEducation: string;
+
+  skill: string;
+}
